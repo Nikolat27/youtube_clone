3bc2e785c1ee9ac5d33ff0dd5c38b8ac87f5130a
Nikolat27 <samalizadeh899@gmail.com>
2024-12-11 02:35:40 +0330

    I added video creating and tomorrow im gonna finish it

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import '@/assets/css/main.css'
-import '@/assets/css/video-player.css'
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import App from './App.vue'
 import router from './router'
